dc67ebcec008440f78d9d2d0aea115e7d61674c0
Sheshan Fernando <sheshan@Sheshans-MacBook-Air.local>
2024-09-03 12:50:43 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ const App = () => {
   return (
     <div>
       <ConnectFour />
-      <p>This is the Connect Four game built with React.</p>
-      <p>
-        <a href="https://github.com/johndoe/connect-four-react">
-          View the source code on GitHub
-        </a>
-      </p>
     </div>
   );
 };
